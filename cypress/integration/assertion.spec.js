2be287889bf14cda096725a54d294b34f95a844c
leo.liang <leo.liang@vendhq.com>
2021-01-10 21:00:52 +1300

    initial commit for cypress project

---
+++ cypress/integration/assertion.spec.js
@@ -0,0 +1,9 @@
+describe('assertion example',()=>{
+  it('assertion true should be equal to true', ()=>{
+    expect(true).to.equal(true)
+  })
+
+  // it('assertion true should be equal to true', ()=>{
+  //   expect(true).to.equal(true)
+  // })  
+}) 
