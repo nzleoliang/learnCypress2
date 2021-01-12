920f3b0e2d1eb528dc18c9d70aaa9c392f4beb89
leo.liang <leo.liang@vendhq.com>
2021-01-13 00:24:24 +1300

    new tests

--- cypress/integration/assertion.spec.js
+++ cypress/integration/assertion.spec.js
@@ -2,8 +2,4 @@ describe('assertion example',()=>{
   it('assertion true should be equal to true', ()=>{
     expect(true).to.equal(true)
   })
-
-  // it('assertion true should be equal to true', ()=>{
-  //   expect(true).to.equal(true)
-  // })  
 }) 
---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,5 @@
+describe("basic testing cases", () => {
+  it("should be able to access the website", () => {
+    cy.visit("http://example.com");
+  });
+});
